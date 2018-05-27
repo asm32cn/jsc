ccaf83c3c784e7986488ecf64c7480dcbec9969c
asm32cn <asm32@qq.com>
2018-05-27 15:16:54 +0800

    js-counting-sort-demo-1--linker.bat ... add

---
+++ jsc-Arithmetic-2018/js-counting-sort-demo-1.js/js-counting-sort-demo-1--linker.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set strLinker=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\jsc.exe
+set strAppName=js-counting-sort-demo-1
+
+echo %strLinker%
+echo.
+
+set strCmd=%strLinker% /target:exe %strAppName%.js
+echo #%strCmd%
+%strCmd%
+
+pause
