0c457eb352085716b074a0303b927d6a27aa2f94
asm32cn <asm32@qq.com>
2018-05-27 16:49:54 +0800

    js-merge-sort-demo-1.js ... modify

--- jsc-Arithmetic-2018/js-merge-sort-demo-1.js/js-merge-sort-demo-1.js
+++ jsc-Arithmetic-2018/js-merge-sort-demo-1.js/js-merge-sort-demo-1.js
@@ -28,7 +28,9 @@ class MergeSortDemo1{
         }
     }
 
+    // 递归实现的归并排序(自顶向下)
     function MergeSortRecursion(data, nLeft, nRight){
+        // 当待排序的序列长度为1时，递归开始回溯，进行merge操作
         if(nLeft == nRight) return;
 
         var nMid = parseInt( (nLeft + nRight) / 2 );
@@ -37,15 +39,21 @@ class MergeSortDemo1{
         Merge(data, nLeft, nMid, nRight);
     }
 
+    // 非递归(迭代)实现的归并排序(自底向上)
     function MergeSortIteration(data){
         var n = data.length;
+        // 子数组索引，前一个为A[left ... mid]，后一个为A[mid + 1 ... right]
         var nLeft, nMid, nRight;
+        // 子数组的大小i初始为1，每轮翻倍
         for(var i = 1; i < n; i *= 2){
             nLeft = 0;
+            // 后一个子数组存在(需要归并)
             while(nLeft + i < n){
                 nMid = nLeft + i - 1;
+                // 后一个子数组大小可能不够
                 nRight = nMid + i < n ? nMid + i : n - 1;
                 Merge(data, nLeft, nMid, nRight);
+                // 前一个子数组索引向后移动
                 nLeft = nRight + 1;
             }
         }
