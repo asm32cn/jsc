1428cb696082fe1bdb22b2d7134498f7ab6d07b5
asm32 <asm32@qq.com>
2018-06-04 10:39:12 +0800

    js-radix-sort-demo-1.js ... modify

--- jsc-Arithmetic-2018/js-radix-sort-demo-1.js/js-radix-sort-demo-1.js
+++ jsc-Arithmetic-2018/js-radix-sort-demo-1.js/js-radix-sort-demo-1.js
@@ -4,52 +4,52 @@ import System;
 import Accessibility;
 
 class LsdRedixSortDemo1{
-    private const DN = 3;
-    private const K = 10;
-    private const radix = [1, 1, 10, 100];
-    private var C = new Array(K);
+	private const DN = 3;
+	private const K = 10;
+	private const radix = [1, 1, 10, 100];
+	private var C = new Array(K);
 
-    function DisplayData(data){
-        Console.WriteLine(data.join(', '));
-    }
+	function DisplayData(data){
+		Console.WriteLine(data.join(', '));
+	}
 
-    function LsdRedixSort(data){
-        for(var d = 1; d < DN; d++){
-            CountingSort(data, d);
-        }
-    }
+	function LsdRedixSort(data){
+		for(var d = 1; d < DN; d++){
+			CountingSort(data, d);
+		}
+	}
 
-    function CountingSort(data, d){
-        function GetDigit(x, d){
-            return parseInt(x / radix[d]) % 10;
-        }
+	function CountingSort(data, d){
+		function GetDigit(x, d){
+			return parseInt(x / radix[d]) % 10;
+		}
 
-        var n = data.length;
-        for(var i = 0; i < K; i++){
-            C[i] = 0;
-        }
-        for(var i = 0; i < n; i++){
-            C[GetDigit(data[i], d)]++;
-        }
-        for(var i = 1; i < K; i++){
-            C[i] += C[i - 1];
-        }
-        var B = new Array(n);
-        for(var i = n - 1; i >= 0; i--){
-            B[--C[GetDigit(data[i], d)]] = data[i];
-        }
-        for(var i = 0; i < n; i++){
-            data[i] = B[i];
-        }
-    }
+		var n = data.length;
+		for(var i = 0; i < K; i++){
+			C[i] = 0;
+		}
+		for(var i = 0; i < n; i++){
+			C[GetDigit(data[i], d)]++;
+		}
+		for(var i = 1; i < K; i++){
+			C[i] += C[i - 1];
+		}
+		var B = new Array(n);
+		for(var i = n - 1; i >= 0; i--){
+			B[--C[GetDigit(data[i], d)]] = data[i];
+		}
+		for(var i = 0; i < n; i++){
+			data[i] = B[i];
+		}
+	}
 }
 
 (function(){
-    // var data = [41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36];
-    var data = [76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82];
+	// var data = [41, 67, 34, 0, 69, 24, 78, 58, 62, 64, 5, 45, 81, 27, 61, 91, 95, 42, 27, 36];
+	var data = [76, 11, 11, 43, 78, 35, 39, 27, 16, 55, 1, 41, 24, 19, 54, 7, 78, 69, 65, 82];
 
-    var lrsd = new LsdRedixSortDemo1();
-    lrsd.DisplayData(data);
-    lrsd.LsdRedixSort(data);
-    lrsd.DisplayData(data);
+	var lrsd = new LsdRedixSortDemo1();
+	lrsd.DisplayData(data);
+	lrsd.LsdRedixSort(data);
+	lrsd.DisplayData(data);
 })();
